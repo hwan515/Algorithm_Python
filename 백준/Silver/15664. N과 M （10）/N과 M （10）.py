d40c314495130a2773197df081dd3a1d89637590
hwan515 <142598961+hwan515@users.noreply.github.com>
2025-09-14 18:45:50 +0900

    [Silver II] Title: N과 M (10), Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,23 @@
+def backtracking(idx):
+    if len(result) == M:
+        print(*result)
+        return
+
+    remember = 0
+
+    for i in range(idx, N):
+        if visited[i] == 0 and remember != arr[i]:
+            visited[i] = 1
+            result.append(arr[i])
+            remember = arr[i]
+            backtracking(i + 1)
+            visited[i] = 0
+            result.pop()
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+visited = [0] * N
+
+result = []
+backtracking(0)
