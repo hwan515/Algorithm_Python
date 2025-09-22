729153375ef8457869dc27ec7048b055ba7780a4
hwan515 <142598961+hwan515@users.noreply.github.com>
2025-09-22 21:43:31 +0900

    [Silver III] Title: 계단 오르기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,14 @@
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+
+if len(arr) <= 2:
+    print(sum(arr))
+else:
+    dp = [0] * N
+    dp[0] = arr[0]
+    dp[1] = arr[0] + arr[1]
+
+    for i in range(2, N):
+        dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i])
+
+    print(dp[N - 1])
