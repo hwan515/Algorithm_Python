322a4037d819ef2ccb453d3215970c3c3b572e49
hwan515 <142598961+hwan515@users.noreply.github.com>
2025-11-11 22:25:11 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 3028 ms, Memory: 129568 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+prefix = [[0] * (N + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        prefix[i][j] = (
+            graph[i-1][j-1]
+            + prefix[i-1][j]
+            + prefix[i][j-1]
+            - prefix[i-1][j-1]
+        )
+
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = (
+        prefix[x2][y2]
+        - prefix[x1 - 1][y2]
+        - prefix[x2][y1 - 1]
+        + prefix[x1 - 1][y1 - 1]
+    )
+    print(result)
