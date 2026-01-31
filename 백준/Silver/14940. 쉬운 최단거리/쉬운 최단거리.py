7307d53cd7554107d9c7454e0d2d106aaed69535
hwan515 <142598961+hwan515@users.noreply.github.com>
2026-02-01 00:24:51 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 208 ms, Memory: 121124 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,38 +1,36 @@
 from collections import deque
+
 def bfs(si, sj):
     q = deque([(si, sj)])
-    directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-    matrix[si][sj] = 0
-    visited[si][sj] = True
+    directions = [[-1, 0], [1, 0], [0, -1], [0 ,1]]
     while q:
         i, j = q.popleft()
         for di, dj in directions:
             ni, nj = i + di, j + dj
-            if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj] and matrix[ni][nj] == 1:
-                matrix[ni][nj] = matrix[i][j] + 1
-                visited[ni][nj] = True
+            if 0 <= ni < n and 0 <= nj < m and answer[ni][nj] == -1 and matrix[ni][nj] != 0:
+                answer[ni][nj] = answer[i][j] + 1
                 q.append((ni, nj))
 
-
-N, M = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False]*M for _ in range(N)]
+n, m = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+answer = [[-1] * m for _ in range(n)]
 
 found = False
-for i in range(N):
-    for j in range(M):
+for i in range(n):
+    for j in range(m):
         if matrix[i][j] == 2:
+            answer[i][j] = 0
             bfs(i, j)
             found = True
             break
     if found:
         break
 
-# 다시 결과를 순회하면서 1을 -1로 변환
-for i in range(N):
-    for j in range(M):
-        if matrix[i][j] == 1 and not visited[i][j]:
-            matrix[i][j] = -1
+for i in range(n):
+    for j in range(m):
+        if matrix[i][j] == 0:
+            answer[i][j] = 0
 
-for row in matrix:
+for row in answer:
     print(*row)
+
